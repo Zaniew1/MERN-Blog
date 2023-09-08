65a21d0b482ccb94c2a60d8de6a7daca614422f8
Mateusz <m.zaniewski1995@gmail.com>
2023-09-08 20:24:10 +0200

    add component newestPost

---
+++ client/src/components/Post/NewestPost.tsx
@@ -0,0 +1,19 @@
+export const NewestPost:React.FC = ():JSX.Element =>{
+    return (
+        <article className="mt-[100px]">
+            <p>Newest Post</p>
+            <h3>Lorem ipsum some headerasdasd aslkdj lksd</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus architecto nihil similique sed explicabo, dolor</p>
+            <div>
+                <div>
+                    <div>obrazek okrągły</div>
+                </div>
+                <div>
+                    <p>Nazwisko autora</p>
+                    <p>Data stworzenia postu</p>
+                </div>
+            </div>
+            <div>duży obrazek</div>
+        </article>
+    )
+}
