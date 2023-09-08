e8b8821fdb8bfc9ba03ef38bbacd73c1e4a95c0c
Mateusz <m.zaniewski1995@gmail.com>
2023-09-08 20:25:03 +0200

    change name to style files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
-import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import {Home} from './components/Home/Home'
-import {Login} from './components/Login/Login'
-import '../dist/App.css';
+import './index.css';
 function App() {
 
   return (
     <Routes>
       <Route index element={<Home/>}></Route>
-      <Route path={'/login'} element={<Login/>}></Route>
+      {/* <Route path={'/login'} element={<Login/>}></Route> */}
     </Routes>
     
 )
