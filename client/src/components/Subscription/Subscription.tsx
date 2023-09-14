5863d4116af0160cc3e5226aec8e0f4599e01f5b
Mateusz <m.zaniewski1995@gmail.com>
2023-09-14 22:24:55 +0200

    add subscription section

---
+++ client/src/components/Subscription/Subscription.tsx
@@ -0,0 +1,9 @@
+export const Subscription:React.FC = ():JSX.Element => {
+    return(
+        <section className="w-full px-[10px] py-[50px] h-[200px] flex flex-col justify-center items-center">
+            <a href="#" className="text-[1.6em] font-bold pt-[45px]  text-[#2C3241] 	">NAZWA</a>
+            <p className="text-[1.2em] text-gray-500 tracking-wider py-[10px] px-[10px] text-center font-semibold">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem eos ratione eligendi magnam odit dignissimos.</p>
+            <input type="email"></input>
+        </section>
+    );
+}
