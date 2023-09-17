92f6f568b2823f8feb3f06894a9f4371245beffb
Mateusz <m.zaniewski1995@gmail.com>
2023-09-17 13:15:39 +0200

    add new component

---
+++ client/src/components/Buttons/SeeMoreButton.tsx
@@ -0,0 +1,7 @@
+export const SeeMoreButton:React.FC = ():JSX.Element => {
+    return (
+        <div className="w-full flex justify-center items-center ">
+            <button className="w-full mx-[2em] my-[2em] border border-cyan-700 text-[1.2em] font-semibold py-[0.5em] shadow-myShadow hover:bg-cyan-700 hover:text-white">See more posts</button>
+        </div>
+    );
+}
