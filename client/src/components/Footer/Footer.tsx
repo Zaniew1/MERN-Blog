418832ac9d74dfb14782b693531fea882e9c31fe
Mateusz <m.zaniewski1995@gmail.com>
2023-09-17 13:16:24 +0200

    add structure and styles

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 export const Footer:React.FC = ():JSX.Element  =>{
     return(
-        <div className="w-full h-[100px] bg-slate-300">
-            
+        <div className="w-full flex justify-center items-center text-slate-400 text-[0.8em] pb-[25px]">
+            <a href="#" target="_blank" className="hover:text-cyan-700">Terms</a>
+            <a href="#" target="_blank" className="px-[20px] hover:text-cyan-700">Privacy Policy</a>
+            <a href="#" target="_blank" className="hover:text-cyan-700">Support</a>
         </div>
     );
 }
--- client/src/components/Socials/Socials.tsx
+++ client/src/components/Socials/Socials.tsx
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faSquareFacebook, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 export const Socials:React.FC = ():JSX.Element  =>{
     return(
-        <div className="w-full h-[100px] bg-slate-600">
-
+        <div className="w-full flex justify-center items-center text-slate-400 py-[15px] text-[1.4em]">
+            <a href="#" target="_blank"> <FontAwesomeIcon icon={faLinkedinIn} className="hover:text-slate-600"/></a>
+            <a href="#" target="_blank"><FontAwesomeIcon icon={faSquareFacebook} className="px-[20px] hover:text-slate-600"/></a>
+            <a href="#" target="_blank"><FontAwesomeIcon icon={faXTwitter} className="hover:text-slate-600"/></a>
         </div>
     );
 }
