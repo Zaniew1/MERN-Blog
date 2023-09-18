e9143af74b52a753878370af7bb56b48f426e8da
Mateusz <m.zaniewski1995@gmail.com>
2023-09-18 20:47:06 +0200

    start blog routes

---
+++ server/routes/blogRoutes.tsx
@@ -0,0 +1,10 @@
+
+// Creating routes to endpoints
+import express from  'express';
+// import userController from  '../controllers/userController'
+// import authController from  '../controllers/authController'
+
+
+const router = express.Router();
+// router.get('/', userController.getAllUsers);
+// router.get('/users/:id', userController.getUser);
