c24c61babed72127ff3bed1b1de56d19a076089b
Mateusz <m.zaniewski1995@gmail.com>
2023-09-18 20:46:31 +0200

    add blog and user models

--- server/models/blogModel.tsx
+++ server/models/blogModel.tsx
@@ -1,6 +1,46 @@
-import bcrypt from 'bcryptjs'
 import mongoose from 'mongoose';
-import crypto from 'crypto'
 
-const BlogSchema = new mongoose.Schema({
+type BlogSchemaType = {
+    title: string, 
+    summary: string,
+    content:string,
+    mainPicture: File,
+    creationDate: number,
+    creator?:string,
+    creatorAvatar?: File
+}
+
+const BlogSchema = new mongoose.Schema<BlogSchemaType>({
+    title:{
+        type:String,
+        required: true,
+        minlength: 8,
+    },
+    summary:{
+        type:String,
+        required: true,
+        minlength: 8,
+    },
+    content:{
+        type:String,
+        required: true,
+        maxlength: 5000,
+    },
+    mainPicture:{
+        required: true,
+        type: File
+    },
+    creationDate:{
+        default:(new Date().getTime()),
+        type: Number,
+    },
+    creator:{
+        type:String,
+    },
+    creatorAvatar:{
+        type: File,
+    },
 })
+const BlogModel = mongoose.model("posts", BlogSchema);
+
+export default BlogModel;
---
+++ server/models/userModel.tsx
@@ -0,0 +1,52 @@
+import bcrypt from 'bcryptjs'
+import mongoose from 'mongoose';
+import crypto from 'crypto'
+
+type UserSchemaType = {
+    email: string, 
+    password: string,
+    confirmPassword:string,
+}
+
+const UserSchema = new mongoose.Schema<UserSchemaType>({
+    email:{
+        type:String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlenght: 8,
+    },
+    confirmPassword:{
+        type: String,
+        required: true,
+        select: false ,
+        validate:{
+            validator: function(el ){
+                return el === this.password; 
+            },
+            message: "Password are not the same"
+        },
+        minlenght: 8,
+    },
+})
+
+
+UserSchema.methods.comparePasswords = async function( typedPassword:string, databasePassword:string){
+    // this function compares two passwords, one password is hashed, one is not,
+    // comparizon is made by hashing first password and then comparing both hashed passwords
+    return await bcrypt.compare(typedPassword, databasePassword);
+}
+UserSchema.pre('save' , async function(next){
+    // isModified is mongoose function
+    if(!this.isModified('password')) return next();
+    // 12 value stands for how strong encryption will be
+     this.password = await bcrypt.hash(this.password, 12); 
+     this.confirmPassword = '';
+    next(); 
+})
+
+const UserModel = mongoose.model("users", UserSchema);
+
+export default UserModel;
