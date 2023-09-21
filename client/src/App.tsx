20ed4295c40bb416cdfac39942baf4e5dd35cfdb
Mateusz <m.zaniewski1995@gmail.com>
2023-09-21 22:37:25 +0200

    add new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import {Home} from './components/Home/Home'
-import {Login} from './components/Login/Login'
+import {Home} from './components/Main/Home'
+import {Login} from './components/Auth/Login'
+import {Forget} from './components/Auth/Forget'
+import {Reset} from './components/Auth/Reset'
 import './index.css';
 import './root.css';
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route index element={<Home/>}></Route>
       <Route path={'/login'} element={<Login/>}></Route>
+      <Route path={'/forget'} element={<Forget/>}></Route>
+      <Route path={'/reset'} element={<Reset/>}></Route>
     </Routes>
     
 )
