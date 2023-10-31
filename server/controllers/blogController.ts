863a9729db3803a4d05ca227073071f9950ed5e5
Mateusz <m.zaniewski1995@gmail.com>
2023-10-31 17:39:08 +0100

    add create new post controller

--- server/controllers/blogController.ts
+++ server/controllers/blogController.ts
@@ -3,27 +3,54 @@ import catchAsync from '../utils/catchAsync';
 import express, { RequestHandler } from 'express';
 import  AppError from '../utils/appError';
 import BlogModel from '../models/blogModel';
-
+import {BlogSchemaType} from '../models/blogModel';
+import  { MongoClient } from "mongodb";
+import 'dotenv/config';
 
 export const createNewArticle: RequestHandler = catchAsync( async (req:express.Request, res:express.Response, next: express.NextFunction) => {
-    res.json('test 1');
+    const {title, summary, content} = req.body as BlogSchemaType;
+    if(!title  || !summary || !content){
+        return next(new AppError('There is are not information', 404))
+    }
+    const newPost = await BlogModel.create(req.body)
+    res.status(201).json({
+        status: "success",
+        data:{
+            post: newPost
+        }
+    });
 })
 
 export const deleteArticle: RequestHandler<{id:string}> = catchAsync( async (req:express.Request, res:express.Response, next: express.NextFunction) => {
-    const id = req.params.id;
-    res.json('test 2');
+    await BlogModel.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+
 
 })
 
 export const editArticle: RequestHandler<{id:string}> = catchAsync( async (req:express.Request, res:express.Response, next: express.NextFunction) => {
     const id = req.params.id;
-    res.json('test 3');
+    const post = await BlogModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true
+    });
+    
+  
+    if(!post){
+        return next(new AppError('There is no such post', 404))
+    }
+    else{
+        res.json('test 3');
+    }           
 
 })
 
 export const getAllArticles: RequestHandler = catchAsync( async (req:express.Request, res:express.Response, next: express.NextFunction) => {
-    const posts = await BlogModel.find()
-    console.log(posts)
+    const posts = await BlogModel.find();
     res.json({
         status: 200,
         posts
@@ -32,6 +59,14 @@ export const getAllArticles: RequestHandler = catchAsync( async (req:express.Req
 
 export const getArticle: RequestHandler<{id:string}> = catchAsync( async (req:express.Request, res:express.Response, next: express.NextFunction) => {
     const id = req.params.id;
-    res.json('test getOneArticle');
+    const post = await BlogModel.findById(id);
+    if(!post){
+        return next(new AppError('There is no such post', 404))
+    }
+    res.status(200).json({
+        status: "success",
+        post
+    });
 
 })
+
