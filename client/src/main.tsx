8bd797bd4762fb42744d4bc319c46cbec859a6d7
Mateusz <m.zaniewski1995@gmail.com>
2023-11-04 21:10:16 +0100

    change context provider inheritance

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,9 +6,9 @@ import {BrowserRouter} from 'react-router-dom';
 import { AuthContextProvider } from './store/Auth-context.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
     <AuthContextProvider>
-        <App />
+      <BrowserRouter>
+            <App />
+      </BrowserRouter>
     </AuthContextProvider>
-  </BrowserRouter>
 )
