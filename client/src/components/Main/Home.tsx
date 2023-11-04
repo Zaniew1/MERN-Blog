90a07311c2a0843d403e8dfa313811ae5e548724
Mateusz <m.zaniewski1995@gmail.com>
2023-11-04 21:13:24 +0100

    add listing posts from database

--- client/src/components/Main/Home.tsx
+++ client/src/components/Main/Home.tsx
@@ -1,27 +1,55 @@
 
-import { Nav } from "./Nav";
+import {ContainerCard } from "../Utilities/ContainerCard";
 import {NewestPost} from '../Post/NewestPost'
 import {NextPost} from '../Post/NextPost'
 import {Subscription} from '../Subscription/Subscription'
 import {Socials} from '../Socials/Socials'
 import {Footer} from '../Footer/Footer'
 import {SeeMoreButton} from '../Buttons/SeeMoreButton'
+import {useEffect, useState} from 'react';
+ type PostsType = {
+    _id: number,
+    title:string,
+    summary: string,
+    content: string,
+    contentCategory: string,
+    mainPicture:string,
+    creationDate: number,
+ }
+
 export const Home = () => {
+    const [posts , setPosts] = useState<PostsType[]>([]);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch("http://localhost:3001/article", {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              });
+              const data = await  response.json();
+              setPosts(data.posts)
+        };
+        fetchPosts();
+      }, []);
+
     return (
-        <main  className='w-full bg-slate-50  overflow-x-hidden flex justify-center '>
-            <div className='w-full flex flex-col relative items-center lg:flex-wrap lg:flex-row lg:justify-center max-w-6xl '>
-                <Nav/>
-                <NewestPost/>
-                <NextPost index={1} header={''} summary={''} content={''} creationDate={123} mainPicture={''} creator={''} creatorAvatar={''} />
-                <NextPost index={2} header={''} summary={''} content={''} creationDate={123} mainPicture={''} creator={''} creatorAvatar={''}/>
-                <NextPost index={3} header={''} summary={''} content={''} creationDate={123} mainPicture={''} creator={''} creatorAvatar={''}/>
-                <NextPost index={4} header={''} summary={''} content={''} creationDate={123} mainPicture={''} creator={''} creatorAvatar={''}/>
-                <NextPost index={5} header={''} summary={''} content={''} creationDate={123} mainPicture={''} creator={''} creatorAvatar={''}/>
+            <ContainerCard>
+                {posts.map((el, index)=>{
+                    const {_id, title, summary, content, contentCategory, mainPicture, creationDate} = el as PostsType;
+                       return(
+                           index === 0 
+                            ?
+                                <NewestPost key={_id} id={_id} title={title} summary={summary} content={content} creationDate={creationDate} mainPicture={mainPicture} contentCategory={contentCategory} creator={'Mateusz Zaniewski'} creatorAvatar={'ja'}/>
+                            :
+                                <NextPost key={_id} index={index} id={_id} title={title} summary={summary} content={content} contentCategory={contentCategory} creationDate={creationDate} mainPicture={mainPicture} creator={''} creatorAvatar={'ja'} />
+                       )
+
+                })}
                 <SeeMoreButton/>
                 <Subscription/>
                 <Socials/>
                 <Footer/>
-            </div>
-        </main>
+            </ContainerCard>
     );
 }  
