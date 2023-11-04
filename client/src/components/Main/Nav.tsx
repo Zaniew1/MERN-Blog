fc8c43092af2c06dd58144654dfd3f3f954e41c1
Mateusz <m.zaniewski1995@gmail.com>
2023-11-04 21:14:05 +0100

    add success and error data handling

--- client/src/components/Main/Nav.tsx
+++ client/src/components/Main/Nav.tsx
@@ -1,5 +1,8 @@
 import {Socials} from '../Socials/Socials'
+import { useContext } from "react";
+import {AuthContext} from '../../store/Auth-context';
 export const Nav = () => {
+    const {loggedIn, setloggedIn} = useContext(AuthContext);
     return (
             <nav className=' flex w-full items-center justify-center top-0 left-0 h-[60px] fixed shadow-myShadow z-50 bg-slate-50  lg:h-[70px]'>
                 <div className="max-w-6xl flex w-full items-center h-full lg:justify-between">
@@ -10,7 +13,11 @@ export const Nav = () => {
                             <p className="text-[1.4em] ml-[10px] text-black opacity-40 h-full"> Blog</p>
                         </a>
                     </header>
-                    <a  href="/login" className="flex justify-center items-center text-[1em] ml-[auto] mr-[20px] text-black opacity-40 h-full hover:text-neutral-500  ">Zaloguj</a>
+                    {!loggedIn && <a  href="/login" className="flex justify-center items-center text-[1em] ml-[auto] mr-[20px] text-black opacity-40 h-full hover:text-neutral-500  ">Zaloguj</a>}
+                    {loggedIn && <a className="flex justify-center items-center text-[1em] ml-[auto] mr-[20px] text-black opacity-40 h-full cursor-pointer hover:text-neutral-500" onClick={()=>{setloggedIn(false); } }>Wyloguj</a>}
+                    {loggedIn && <a href="/me" className="w-[40px] h-[40px] rounded-[50%] cursor-pointer md:w-[30px] md:h-[30px] ">
+                                    <img className="w-full h-full rounded-[50%]" src={`src/assets/${'ja'}.jpg`}/>
+                                 </a>}
                     <Socials customStyle={" hidden w-[15%] justify-center items-center text-slate-400 py-[15px] text-[1.4em] lg:flex"}/>
                 </div>
             </nav>
