e4bc598935320723e452e809771d634a2230f971
Mateusz <m.zaniewski1995@gmail.com>
2023-11-04 21:11:17 +0100

    add error and success handling after creatingNewUs

--- client/src/components/Auth/Create.tsx
+++ client/src/components/Auth/Create.tsx
@@ -1,10 +1,17 @@
-import {Nav} from '../Main/Nav';
+import { ContainerCard } from "../Utilities/ContainerCard";
 import { useState} from 'react';
+import { useNavigate } from 'react-router-dom'
+import { Popup } from '../Utilities/Popup';
+import { useContext } from "react";
+import {AuthContext} from '../../store/Auth-context'
 export const Create:React.FC = ():JSX.Element => {
+    const navigate = useNavigate();
+    const {setloggedIn} = useContext(AuthContext);
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [confirmPassword, setConfirmPassword] = useState<string>('');
     const [error, setError] = useState<string>('');
+    const [success, setSuccess] = useState<string>('');
     const Login = async (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault(); 
         if(email === ""){
@@ -27,28 +34,34 @@ export const Create:React.FC = ():JSX.Element => {
                 const message = `An error has occured: ${response.status}`;
                 setError(message);
               }
+              else{
+                setloggedIn(true);
+                setSuccess('Udało się stworzyć użytkownika!');
+                setTimeout(()=>{
+                  navigate('/');
+                },1000)
+              }
             const user = await response.json();
             console.log(user)
     }
     
     
     return (
-        <div className="flex justify-center">
-            <Nav/>
-            <div className="flex items-center flex-col justify-center w-screen h-screen md:w-[60%] lg:w-[35%] xl:w-[25%]">
-                <p className="text-[1.6em] ml-[0.7em] font-bold text-[#2C3241]">Zaloguj się</p>
-                <form className="w-[70%] flex flex-col justify-center mt-[20px]" onSubmit={Login}>
-                    <label className="text-[1em] font-semibold py-[5px]" htmlFor="email">Email</label>
-                    <input className="px-[10px] py-[10px] border-[1px] border-cyan-700" name="email" id="email" type="text" onChange={(e)=>{setEmail(e.target.value); setError('');}} value={email} placeholder="Podaj email" />
-                    <label className="text-[1em] font-semibold py-[5px]" htmlFor="password">Hasło</label>
-                    <input className="px-[10px] py-[10px] border-[1px] border-cyan-700" id="password" onChange={(e)=>{setPassword(e.target.value);  setError('');}} value={password} type="password" placeholder="Podaj hasło" />
-                    <label className="text-[1em] font-semibold py-[5px]" htmlFor="confirmPassword">Potwierdź Hasło</label>
-                    <input className="px-[10px] py-[10px] border-[1px] border-cyan-700" id="confirmPassword" onChange={(e)=>{setConfirmPassword(e.target.value);  setError('');}} value={confirmPassword} type="password" placeholder="Potwierdź hasło" />
-                    <div className="text-[red] text-[0.7em] mt-[15px] text-center">{error ? error : ''}</div>
-                    <button  type="submit" className="w-[80%] m-auto my-[2em] border border-cyan-700 text-[1.2em] font-semibold py-[0.5em] shadow-myShadow hover:bg-cyan-700 hover:text-white">Zaloguj</button>
-                </form>
-                <a href="/forget" className="mt-[20px] text-[0.8em] underline ">Nie pamiętam hasła</a>
-            </div>
+      <ContainerCard>
+        {success && <Popup type={'success'} text={success}/>}
+        <div className="flex items-center flex-col justify-center w-screen h-screen md:w-[60%] lg:w-[35%] xl:w-[25%]">
+            <p className="text-[1.6em] ml-[0.7em] font-bold text-[#2C3241]">Stwórz użytkownika</p>
+            <form className="w-[70%] flex flex-col justify-center mt-[20px]" onSubmit={Login}>
+                <label className="text-[1em] font-semibold py-[5px]" htmlFor="email">Email</label>
+                <input className="px-[10px] py-[10px] border-[1px] border-cyan-700" name="email" id="email" type="text" onChange={(e)=>{setEmail(e.target.value); setError('');}} value={email} placeholder="Podaj email" />
+                <label className="text-[1em] font-semibold py-[5px]" htmlFor="password">Hasło</label>
+                <input className="px-[10px] py-[10px] border-[1px] border-cyan-700" id="password" onChange={(e)=>{setPassword(e.target.value);  setError('');}} value={password} type="password" placeholder="Podaj hasło" />
+                <label className="text-[1em] font-semibold py-[5px]" htmlFor="confirmPassword">Potwierdź Hasło</label>
+                <input className="px-[10px] py-[10px] border-[1px] border-cyan-700" id="confirmPassword" onChange={(e)=>{setConfirmPassword(e.target.value);  setError('');}} value={confirmPassword} type="password" placeholder="Potwierdź hasło" />
+                <div className="text-[red] text-[0.7em] mt-[15px] text-center">{error ? error : ''}</div>
+                <button  type="submit" className="w-[80%] m-auto my-[2em] border border-cyan-700 text-[1.2em] font-semibold py-[0.5em] shadow-myShadow hover:bg-cyan-700 hover:text-white">Stwórz</button>
+            </form>
         </div>
+      </ContainerCard>
     )
 }
