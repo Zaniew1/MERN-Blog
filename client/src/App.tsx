46436f4c12ba2bd1c4192c71ee05857c81b39ff3
Mateusz <m.zaniewski1995@gmail.com>
2023-11-04 21:09:29 +0100

    add changePassword, singlePost, me routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import {Home} from './components/Main/Home'
+import {Me} from './components/Main/Me'
 import {Login} from './components/Auth/Login'
-import {Forget} from './components/Auth/Forget'
+import {ForgetPassword} from './components/Auth/ForgetPassword'
+import {ChangePassword} from './components/Auth/ChangePassword'
 import {Reset} from './components/Auth/Reset'
+import {SinglePost} from './components/Post/SinglePost';
 import { Create } from './components/Auth/Create';
 import './index.css';
 import './root.css';
@@ -12,9 +15,12 @@ function App() {
     <Routes>
       <Route index element={<Home/>}></Route>
       <Route path={'/login'} element={<Login/>}></Route>
-      <Route path={'/forget'} element={<Forget/>}></Route>
+      <Route path={'/forgetPassword'} element={<ForgetPassword/>}></Route>
+      <Route path={'/changePassword'} element={<ChangePassword/>}></Route>
       <Route path={'/reset'} element={<Reset/>}></Route>
       <Route path={'/createUser'} element={<Create/>}></Route>
+      <Route path={'/me'} element={<Me/>}></Route>
+      <Route path={'/:id'} element={<SinglePost/>}></Route>
     </Routes>
     
 )
