71189fa22f21d9c24d487468b34777e6f8ee9f66
Mateusz <m.zaniewski1995@gmail.com>
2023-11-04 21:12:05 +0100

    add userData to Authcontext

--- client/src/store/Auth-context.tsx
+++ client/src/store/Auth-context.tsx
@@ -1,61 +1,52 @@
 import React, { useState } from "react";
+
  type AuthContextType = {
     loggedIn: boolean,
     setloggedIn: (logged: boolean) => void,
     userData: UserDataType,
-    setUserData: (value: object) => void,
+    setUserData: (data: UserDataType) => void
   }
 
-  
+
  type ContextPropsType = {
     children: React.ReactNode
   }
-  export type UserDataType = {
-    status:string,
-    message?:string,
-    token?: string,
-    data?:{
-      user:{
+ type UserDataType = {
         _id:string,
-        username:string,
         email:string,
-        creationDate:number
-      }
-    }
+        creationDate:number,
+        newsletter:boolean
   }
-const userDataEmpty = {
-  status:'',
-  token: '',
-  message: '',
-  data:{
-    user:{
-      _id:'',
-      username:'',
-      email:'',
-      creationDate:0
-    }
-  }
-}
+
 export const AuthContext = React.createContext<AuthContextType>({
     loggedIn: false,
     setloggedIn: () => {false},
-    userData: userDataEmpty,
-    setUserData: () => {''},
+    userData:{
+      _id:"",
+      email:"",
+      creationDate:0,
+      newsletter:false
+    },
+    setUserData: () => {}
   });
 
 
 
 export const AuthContextProvider = (props: ContextPropsType) => {
   const [loggedIn, setloggedIn] = useState<boolean>(false);
-  const [userData, setUserData] = useState<UserDataType>(userDataEmpty);
-
+  const [userData, setUserData] = useState<UserDataType>({
+    _id: "",
+    email: "",
+    creationDate: 0,
+    newsletter: false
+  });
   return (
     <AuthContext.Provider
       value={{
         loggedIn: loggedIn,
         setloggedIn: setloggedIn,
         userData:userData,
-        setUserData: setUserData,
+        setUserData: setUserData
       }}
     >
       {props.children}
