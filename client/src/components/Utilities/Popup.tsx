140205c19b23eda4dc9b0110ae74d9e201bbcbbb
Mateusz <m.zaniewski1995@gmail.com>
2023-11-04 21:12:20 +0100

    add popup for error and success handling

---
+++ client/src/components/Utilities/Popup.tsx
@@ -0,0 +1,13 @@
+
+type PopupType = {
+    text:string,
+    type: "success" | "error"
+}
+export const Popup:React.FC<PopupType> = (props):JSX.Element => {
+
+    return (
+        <div className={` absolute ${props.type == "success" ? "bg-green-400" :  "bg-red-400" }   top-[15%] l-[50%] transform translate-y-[-50%] rounded flex justify-center items-center max-w-[400px] `}>
+            <p className="text-white text-center px-[15px] py-[15px]">{props.text}</p>
+        </div>
+    );
+}
