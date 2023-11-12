ff6d2d77c3f9566c7f002e167a7a22ddd6d767c0
Mateusz <m.zaniewski1995@gmail.com>
2023-11-12 11:14:30 +0100

    add delete and edit buttons

--- client/src/components/Post/NewestPost.tsx
+++ client/src/components/Post/NewestPost.tsx
@@ -1,4 +1,8 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {useContext} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../store/Auth-context';
 type NewestPostType = {
     key:number,
     id: number,
@@ -12,14 +16,28 @@ type NewestPostType = {
     creatorAvatar:string
 }
 export const NewestPost:React.FC<NewestPostType> = (props):JSX.Element =>{
-    const date = new Date(props.creationDate).toLocaleDateString()
-    console.log(props)
+    const navigator = useNavigate();
+    const date = new Date(props.creationDate).toLocaleDateString();
+    const {loggedIn, userData} = useContext(AuthContext);
+    const deletePost = async () => {
+        const response = await fetch("http://localhost:3001/article/"+props.id, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                  },
+              })
+              if(response.ok){
+                  const data = await response.json();
+                  console.log(data)
+                  navigator('/');
+              }
+    }
+
     return (
         <article className=" flex flex-col w-[95%] mx-auto   pb-[25px] mt-[100px] border-b border-solid border-slate-300 z-10 md:flex-row md:border-none lg:flex-grow lg:mx-[15px]">
-            <a href={`/${props.id}`} className="flex flex-col w-full h-full md:flex-row lg:flex-grow ">
                 <div className="w-full md:w-[50%] lg:flex-col lg:flex lg:justify-center mr-[15px]">
                     <p className="tracking-wide font-bold text-[1.2em] pb-[15px] text-[#2C3241] md:mb-[15px]">Newest Post</p>
-                    <h3 className="text-[2.3em] font-bold cursor-pointer transition ease-in-out text-[#2C3241] leading-tight md:mb-[25px] md:text-[3.4em] md:leading-[1.1em] hover:text-sky-700">{props.title}</h3>
+                    <a href={`/${props.id}`} className="text-[2.3em] font-bold cursor-pointer transition ease-in-out text-[#2C3241] leading-tight md:mb-[25px] md:text-[3.4em] md:leading-[1.1em] hover:text-sky-700">{props.title}</a>
                     <p className="text-[1.2em] text-gray-500 tracking-wider py-[10px] md:pr-[50px] md:font-medium">{props.summary}</p>
                     <div className="w-full  my-[15px] flex">
                         <div className="w-[30%] h-full flex justify-center items-center md:justify-start md:w-[100px]">
@@ -27,16 +45,21 @@ export const NewestPost:React.FC<NewestPostType> = (props):JSX.Element =>{
                                 <img className="w-full h-full rounded-[50%]" src={`http://localhost:3001/images/ja.jpg`}/>
                             </div>
                         </div>
-                        <div className=" w-[70%] ">
+                        <div className=" w-[40%] ">
                             <p className="text-[1em] font-bold text-[#2C3241]">{props.creator ? props.creator : "Mateusz Zaniewski"}</p>
                             <p className="text-[1.2em] font-thin tracking-wider text-gray-400">{date}</p>
                         </div>
+                    {userData.email == "m.zaniewski1995@gmail.com" && loggedIn &&
+                        <div className=" w-[30%] flex text-[1.5em] text-[#2C3241]">
+                            <a className="cursor-pointer" href={`/editPost/${props.id}`} ><FontAwesomeIcon icon={faPenToSquare} className="px-[20px] hover:text-slate-600"/></a>
+                            <div className="cursor-pointer" onClick={deletePost} ><FontAwesomeIcon icon={faTrash} className="hover:text-slate-600"/></div>
+                        </div>
+}
                     </div>
                 </div>
-                <div className="w-full aspect-square cursor-pointer border md:w-[50%] my-auto lg:ml-[15px] ">
+                <a href={`/${props.id}`} className="w-full aspect-square cursor-pointer border md:w-[50%] my-auto lg:ml-[15px] ">
                     <img className="w-full h-full " src={`http://localhost:3001/${props.mainPicture}`}/>
-                </div>
-            </a>
+                </a>
         </article>
     )
 }
--- client/src/components/Post/NextPost.tsx
+++ client/src/components/Post/NextPost.tsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import {  faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { AuthContext } from '../../store/Auth-context';
 type NextPostType = {
     key:number,
     id: number,
@@ -12,16 +17,35 @@ type NextPostType = {
     creatorAvatar?:string
 }
 export const NextPost:React.FC<NextPostType> = (props):JSX.Element => {
+    const navigator = useNavigate();
     const date = new Date(props.creationDate).toLocaleDateString();
-    console.log(props.mainPicture)
+    const {loggedIn, userData} = useContext(AuthContext);
+
+    const deletePost = async () => {
+        const response = await fetch("http://localhost:3001/article/"+props.id, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+              },
+          })
+          console.log(response)
+          if(response.ok){
+              const data =  await response.json()
+              console.log(data)
+              navigator('/')
+          }else{
+            console.log("Nie udało się usunąć")
+          }
+
+    }
+ 
     return(
         <article className={`mt-[20px] mx-[15px] z-10 border-b border-solid border-slate-300 md:border-none md:mb-[60px] ${props.index > 2 ? "lg:w-[25%]" : "lg:w-[45%]"} lg:flex-grow `}>
-            <a href={`/${props.id}`}>
-                <div className={`w-full h-[200px] border bg-center cursor-pointer bg-cover md:aspect-video md:h-auto ${props.index > 2 ? "lg:aspect-video" : "lg:h-[300px]"}`}>
+                <a href={`/${props.id}`} className={`w-full h-[200px] border bg-center cursor-pointer bg-cover md:aspect-video md:h-auto ${props.index > 2 ? "lg:aspect-video" : "lg:h-[300px]"}`}>
                     <img className="w-full h-full " src={`http://localhost:3001/${props.mainPicture}`}/>    
-                </div>
+                </a>
                 <p className="tracking-wide font-bold text-[1.2em] py-[15px] text-[#2C3241] md:text-[1em] ">{props.contentCategory}</p>
-                <h3 className="text-[1.6em] cursor-pointer font-bold text-[#2C3241] md:text-[1.8em] md:font-semibold hover:text-sky-700">{props.title}</h3>
+                <a href={`/${props.id}`} className="text-[1.6em] cursor-pointer font-bold text-[#2C3241] md:text-[1.8em] md:font-semibold hover:text-sky-700">{props.title}</a>
                 <p className="text-[1em] text-gray-500 tracking-wider py-[10px] md:text-[1.1em] md:min-h-[100px]">{props.summary}</p>
                 <div className="w-full  my-[15px] flex ">
                     <div className="w-[20%] h-full flex justify-center items-center md:justify-start md:w-[60px]">
@@ -29,12 +53,17 @@ export const NextPost:React.FC<NextPostType> = (props):JSX.Element => {
                             <img className="w-full h-full rounded-[50%]" src={`http://localhost:3001/images/ja.jpg`}/>
                         </div>
                     </div>
-                    <div className=" w-[70%]">
+                    <div className=" w-[40%]">
                         <p className="text-[1em] font-bold text-[#2C3241]">{props.creator ? props.creator : "Mateusz Zaniewski"}</p>
                         <p className="text-[1.2em] font-thin tracking-wider text-gray-400">{date}</p>
                     </div>
+                    {userData.email == "m.zaniewski1995@gmail.com" && loggedIn &&
+                        <div className=" w-[30%] flex">
+                                <a className="cursor-pointer" href={`/editPost/${props.id}`} ><FontAwesomeIcon icon={faPenToSquare} className="px-[20px] hover:text-slate-600"/></a>
+                                <div className="cursor-pointer" onClick={deletePost}><FontAwesomeIcon icon={faTrash} className="hover:text-slate-600"/></div>
+                        </div>
+                    }
                 </div>
-            </a>
         </article>
     )
 }
