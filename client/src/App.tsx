0c51f6bc4c2655215d1be42eaefeed77a2706268
Mateusz <m.zaniewski1995@gmail.com>
2023-11-12 11:15:06 +0100

    overlapping containerCard over whole app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,24 +6,29 @@ import {ForgetPassword} from './components/Auth/ForgetPassword'
 import {ChangePassword} from './components/Auth/ChangePassword'
 import {Reset} from './components/Auth/Reset'
 import {SinglePost} from './components/Post/SinglePost';
+import {EditPost} from './components/Post/EditPost';
 import { Create } from './components/Auth/Create';
 import {CreateNewPost} from './components/Post/CreateNewPost'
+import { ContainerCard } from './components/Utilities/ContainerCard';
 import './index.css';
 import './root.css';
 function App() {
 
   return (
-    <Routes>
-      <Route index element={<Home/>}></Route>
-      <Route path={'/login'} element={<Login/>}></Route>
-      <Route path={'/forgetPassword'} element={<ForgetPassword/>}></Route>
-      <Route path={'/changePassword'} element={<ChangePassword/>}></Route>
-      <Route path={'/reset'} element={<Reset/>}></Route>
-      <Route path={'/createUser'} element={<Create/>}></Route>
-      <Route path={'/me'} element={<Me/>}></Route>
-      <Route path={'/CreateNewPost'} element={<CreateNewPost/>}></Route>
-      <Route path={'/:id'} element={<SinglePost/>}></Route>
-    </Routes>
+    <ContainerCard>
+      <Routes>
+        <Route index element={<Home/>}></Route>
+        <Route path={'/login'} element={<Login/>}></Route>
+        <Route path={'/forgetPassword'} element={<ForgetPassword/>}></Route>
+        <Route path={'/changePassword'} element={<ChangePassword/>}></Route>
+        <Route path={'/reset'} element={<Reset/>}></Route>
+        <Route path={'/createUser'} element={<Create/>}></Route>
+        <Route path={'/me'} element={<Me/>}></Route>
+        <Route path={'/CreateNewPost'} element={<CreateNewPost/>}></Route>
+        <Route path={'/editPost/:id'} element={<EditPost/>}></Route>
+        <Route path={'/:id'} element={<SinglePost/>}></Route>
+      </Routes>
+    </ContainerCard>
     
 )
 }
