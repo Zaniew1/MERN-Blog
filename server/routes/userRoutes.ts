a34ab045dc1a1c05d83fa8210981f63aa8b81ef1
Mateusz <m.zaniewski1995@gmail.com>
2023-11-12 11:11:34 +0100

    add subscribeNewsletter and profile routes

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -9,5 +9,7 @@ userRouter.post('/forgetPassword', authController.forgetPassword);
 userRouter.post('/changePassword', authController.changePassword);
 userRouter.patch('/resetPassword/:token', authController.resetPassword);
 userRouter.post('/createNewUser', authController.createNewUser);
+userRouter.post('/subscribeNewsletter', authController.subscribeNewsletter);
+userRouter.get('/profile', authController.myProfile);
 
 export default userRouter;
