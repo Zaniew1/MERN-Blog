cb42933dfcc784d0870a2bfb1aca8b9dad7380d6
Mateusz <m.zaniewski1995@gmail.com>
2023-11-12 11:13:44 +0100

    change name of nav to nav bar

---
+++ client/src/components/Main/NavBar.tsx
@@ -0,0 +1,29 @@
+
+
+import { Header } from './Header';
+import { Burger } from './Burger';
+import { NavMobile } from './NavMobile';
+import { useContext } from "react";
+import { UIContext } from '../../store/UI-context';
+import {AuthContext} from '../../store/Auth-context';
+import { NavDesktop } from './NavDesktop';
+export const NavBar = () => {
+    const {mobileNav} = useContext(UIContext);
+    const {loggedIn} = useContext(AuthContext);
+    return (
+        <>
+            <nav className=' flex w-full flex-col grow items-center justify-center top-0 left-0 fixed shadow-myShadow z-50 bg-slate-50 lg:h-[70px]'>
+                <div className={`max-w-6xl flex w-full justify-between items-center h-full  ${mobileNav ? "border-b-2" : "border-b-0"} `}>
+                   <Header/>
+                   <Burger/>
+                   <NavDesktop/>
+                </div>
+                <div className={`w-full transition-height ease-out duration-300 bg-slate-50 ${mobileNav ? `${loggedIn ? "h-[200px]" : "h-[100px]" }` : "h-0"} md:hidden`}>
+                     <NavMobile/>
+                </div>
+            </nav>
+     
+        </>
+    
+    );
+}
--- client/src/components/Utilities/ContainerCard.tsx
+++ client/src/components/Utilities/ContainerCard.tsx
@@ -1,6 +1,6 @@
 
 import {ReactNode} from 'react';
-import { Nav } from "../Main/Nav";
+import { NavBar } from "../Main/NavBar";
 
 type ChildrenType = {
     children: ReactNode
@@ -10,7 +10,7 @@ export const ContainerCard:React.FC<ChildrenType> = (props):JSX.Element => {
     return  (
         <main  className='w-full bg-slate-50  overflow-x-hidden flex justify-center '>
             <div className='w-full flex flex-col relative items-center lg:flex-wrap lg:flex-row lg:justify-center max-w-6xl '>
-                <Nav/>
+                <NavBar/>
                 {props.children}
             </div>
         </main>
