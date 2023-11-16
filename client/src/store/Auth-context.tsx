cc6b9efb81e9eb42acafc6de42089cfb5fe366a1
Mateusz <m.zaniewski1995@gmail.com>
2023-11-16 21:45:17 +0100

    change user type after populate, fetch userData

--- client/src/store/Auth-context.tsx
+++ client/src/store/Auth-context.tsx
@@ -1,45 +1,68 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { getCookie } from "../utils/cookies";
  type AuthContextType = {
     loggedIn: boolean,
     setloggedIn: (logged: boolean) => void,
     userData: UserDataType,
     setUserData: (data: UserDataType) => void
   }
-
-
  type ContextPropsType = {
     children: React.ReactNode
   }
  type UserDataType = {
-        _id:string,
+        id:string,
         email:string,
-        creationDate:number,
-        newsletter:boolean
+        name:string,
+        surname:string,
+        avatar?:string
   }
 
 export const AuthContext = React.createContext<AuthContextType>({
     loggedIn: false,
     setloggedIn: () => {false},
     userData:{
-      _id:"",
-      email:"Mateusz Zaniewski",
-      creationDate:0,
-      newsletter:false
+      id:"",
+      email:"",
+      name:"",
+      surname:"",
+      avatar: "",
     },
     setUserData: () => {}
   });
 
 
-
 export const AuthContextProvider = (props: ContextPropsType) => {
   const [loggedIn, setloggedIn] = useState<boolean>(false);
   const [userData, setUserData] = useState<UserDataType>({
-    _id: "",
-    email: "",
-    creationDate: 0,
-    newsletter: false
+    id:"",
+    email:"",
+    name:"",
+    surname:"",
+    avatar: "",
   });
+
+  useEffect(()=>{
+    const fetchUserData = async ()=>{
+      const jwt = getCookie("jwt");
+      const response = await fetch("http://localhost:3001/profile", {
+        method: "GET",
+        headers: {Authorization: `Bearer ${jwt}`}
+      })
+      if (response.ok) {
+        // console.log(response)
+      }else{
+         console.log(response)
+      }
+      const user = await response.json();
+      if(user.status == 'success'){
+        setloggedIn(true)
+        const {id, email, name, surname, avatar} = user
+        setUserData({id, email, name, surname, avatar} as UserDataType);
+      }
+    }
+    fetchUserData();
+
+  },[])
   return (
     <AuthContext.Provider
       value={{
