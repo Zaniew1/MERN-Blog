68718251d24e34e629e45d329c0f9fbbf1b7cf02
Mateusz <m.zaniewski1995@gmail.com>
2023-11-16 21:50:02 +0100

    add protect middleware

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import * as authController from '../controllers/authController';
-
-
+import {protect} from '../middleware/authMiddleware'
 const userRouter = express.Router();
-userRouter.post('/loginUser', authController.loginUser);
-userRouter.post('/logoutUser', authController.logoutUser);
-userRouter.post('/forgetPassword', authController.forgetPassword);
-userRouter.post('/changePassword', authController.changePassword);
-userRouter.patch('/resetPassword/:token', authController.resetPassword);
+
+
 userRouter.post('/createNewUser', authController.createNewUser);
-userRouter.post('/subscribeNewsletter', authController.subscribeNewsletter);
-userRouter.get('/profile', authController.myProfile);
+userRouter.post('/loginUser', authController.loginUser);
+userRouter.post('/forgetPassword', authController.forgetPassword);
+userRouter.post('/logoutUser',protect, authController.logoutUser);
+userRouter.get('/profile', protect, authController.getMe);
+userRouter.post('/changePassword',protect, authController.changePassword);
+userRouter.post('/editUser',protect, authController.editUser);
+userRouter.patch('/resetPassword/:token', authController.resetPassword);
 
 export default userRouter;
