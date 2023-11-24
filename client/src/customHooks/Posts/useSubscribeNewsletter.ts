e5e5cb0c935ebc1f93c11dd99df84665f64b6eb6
Mateusz <m.zaniewski1995@gmail.com>
2023-11-24 21:05:51 +0100

    add useSubscribeNewsletter custom hook

---
+++ client/src/customHooks/Posts/useSubscribeNewsletter.ts
@@ -0,0 +1,20 @@
+import { useShowInfo } from '../useShowInfo';
+export const useSubscribeNewsletter = (email:string) => {
+    const {showError, showSuccess} = useShowInfo()
+    const subscribeNewsletter = async (e: React.FormEvent<HTMLFormElement>): Promise<void> =>{
+            e.preventDefault();
+            const response = await fetch("http://localhost:3001/newsletter", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify( {email} ),
+              })
+              if (!response.ok) {
+                showError("Nie udało się zasubskrybować, spróbuj ponownie później!");
+              }else{
+                showSuccess("Zasubskrybowano, oczekuj na nowe posty!");
+              }
+    }
+    return {subscribeNewsletter}
+}
