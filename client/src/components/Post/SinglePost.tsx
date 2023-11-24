6de228a12a6bb3df58f6f6317eeec1909d4d3b1e
Mateusz <m.zaniewski1995@gmail.com>
2023-11-24 21:04:07 +0100

    add SinglePostimage component

--- client/src/components/Post/SinglePost.tsx
+++ client/src/components/Post/SinglePost.tsx
@@ -1,20 +1,19 @@
 import { ContainerCard } from "../Utilities/ContainerCard";
 import { useGetOnePost } from "../../customHooks/Posts/useGetOnePost";
 import { PostType } from '../../types/blogTypes';
+import { SinglePostImage } from "../Utilities/SinglePostImage";
 export const SinglePost:React.FC = ():JSX.Element =>{
   const {data} = useGetOnePost(window.location.pathname)
   const {title, mainPicture, creator, content, contentCategory, creationDate} = data as PostType
     return (
        <ContainerCard>
-            <div className="flex flex-col justify-center items-center  w-full mt-[150px] ">
-              <div className="text-center text-[1.8em] mb-[20px] text-[#2C3241] font-bold">{contentCategory}</div>
-              <div className="">
-                <img src={`http://localhost:3001/images/posts/${mainPicture}`}  alt="Główne zdjęcie" />
-              </div>
-              <p className="mt-[10px] text-[0.9em] text-slate-700">{`Created by: ${creator.name} ${creator.surname}`}</p>
-              <p className="mt-[10px] text-[0.9em] text-slate-700">{`${new Date(creationDate).toLocaleDateString()}`}</p>
-              <h3 className="text-[2.3em] mt-[30px] text-center font-bold transition ease-in-out text-[#2C3241] leading-tight md:mb-[25px] md:text-[3.4em] md:leading-[1.1em] ">{title}</h3>
-              <div className="my-[40px] w-full px-[15px] text-[0.9em] text-slate-700 text-justify" dangerouslySetInnerHTML={{__html:content}}></div>
+            <div   className="flex flex-col justify-center items-center  w-[90%] mt-[150px] ">
+              <h3 className="text-[2em]  mt-[30px] text-center font-bold transition ease-in-out text-[#2C3241] leading-tight md:mb-[25px] md:text-[3.4em] md:leading-[1.1em] ">{title}</h3>
+              <div  className="text-center text-[1.3em] mb-[20px] text-zinc-500 py-[10px] font-bold">{contentCategory}</div>
+              <SinglePostImage style={`w-full aspect-square cursor-pointer border md:w-[45%] md:flex-grow md:h-[300px] md:mx-[15px]`} mainPicture={mainPicture}/>
+              <p className="mt-[10px] text-[0.9em] font-semibold text-slate-700">{`Created by: ${creator.name} ${creator.surname}`}</p>
+              <p className="mt-[10px] text-[0.9em] font-semibold text-slate-700">{`${new Date(creationDate).toLocaleDateString()}`}</p>
+              <div className=" flex justify-center items-center flex-col my-[40px] px-[15px] text-[0.9em] mb-[100px]  mx-auto md:w-[80%] font-normal text-slate-700 text-justify" dangerouslySetInnerHTML={{__html:content}}></div>
             </div>
        </ContainerCard>
     )
