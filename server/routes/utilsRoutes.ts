38375df96be1e3a0e8fb59c84ef8b74b3ccc3121
Mateusz <m.zaniewski1995@gmail.com>
2023-11-24 21:09:10 +0100

    add utilsRouter

---
+++ server/routes/utilsRoutes.ts
@@ -0,0 +1,8 @@
+import * as utilsController from '../controllers/utilsController';
+import express from 'express';
+const utilsRouter = express.Router(); 
+
+utilsRouter.post('/newsletter', utilsController.newsletter);
+
+    
+export default utilsRouter;
