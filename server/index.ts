3510a1dbc00508bccc90b85717aaacb8e3c030d8
Mateusz <m.zaniewski1995@gmail.com>
2024-01-01 22:44:15 +0100

    fix cors

--- server/index.ts
+++ server/index.ts
@@ -22,19 +22,6 @@ app.use("/images", express.static(__dirname + "/images"));
 
 app.use(cors());
 app.options("*", cors());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", userRouter);
